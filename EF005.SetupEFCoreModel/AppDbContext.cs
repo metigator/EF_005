75de6a8563cad5a1469b264082056c8aac591d9f
Metigator <87314838+metigator@users.noreply.github.com>
2022-10-31 12:21:03 -0400

    Update AppDbContext.cs

--- EF005.SetupEFCoreModel/AppDbContext.cs
+++ EF005.SetupEFCoreModel/AppDbContext.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace EF005.SetupEFCoreModel
 {
